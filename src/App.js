5a8d1dbf46ef785d286e073f3df75a1389c7d4eb
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-14 20:23:04 -0400

    added a constructor and an initialized state to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoggedIn: false,
+			user: {},
+		};
+	}
+
 	render() {
 		return (
 			<BrowserRouter>
