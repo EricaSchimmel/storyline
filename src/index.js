436191fc3980cd6809ad8c349560d7a5210d5a01
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-14 22:03:56 -0400

    configured axios defaults in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import rootReducer from "./redux/reducers/rootReducer";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.xsrfCookieName = "CSRF-TOKEN";
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
 	<React.StrictMode>
