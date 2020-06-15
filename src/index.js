930e23a9787b303d5841e469de5d2b6b22079b22
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-14 22:05:01 -0400

    added one more axios default in index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 axios.defaults.xsrfCookieName = "CSRF-TOKEN";
+axios.defaults.xsrfHeaderName = "X-CSRF-Token";
 axios.defaults.withCredentials = true;
 
 ReactDOM.render(
