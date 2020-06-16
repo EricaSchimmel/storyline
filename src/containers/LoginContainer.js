09d7b9b50fb31848f19bd2f85c686c51f89fd6b2
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-15 22:52:28 -0400

    created starter code for NavContainer and moved the export default to the bottom of the file in NavContainer and LoginContainer

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-
-export default class LoginContainer extends Component {
+class LoginContainer extends Component {
 	render() {
 		return <div></div>;
 	}
 }
+
+export default LoginContainer;
---
+++ src/containers/NavContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class NavContainer extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default NavContainer;
