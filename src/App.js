bcf7e3d75b9b99c165fc2c58fa7baceb6c55c48f
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-15 23:51:44 -0400

    added Login Component to the component in login oute in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Login from "./components/Login";
 import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
 			<BrowserRouter>
 				<Route path="/" exact component={} />
 				<Route path="/signup" exact component={} />
-				<Route path="/login" exact component={} />
+				<Route path="/login" exact component={Login} />
 			</BrowserRouter>
 		);
 	}
