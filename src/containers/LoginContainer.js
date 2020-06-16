100939cf626ce27e04e6468cdda7c3baf46216cb
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-15 23:08:44 -0400

    added intialized state and taking in props and a handleOnChange event for handling changes in the form and added a form in render in LoginContainer

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,7 +1,33 @@
 import React, { Component } from "react";
 class LoginContainer extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			email: "",
+			password: "",
+			errors: [],
+		};
+    }
+    
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
 	render() {
-		return <div></div>;
+		return (
+			<div id="login-container">
+				<form onSubmit={this.handleOnSubmit}>
+                    <label>Email</label></br>
+                    <input type="email" id="email" onChange={this.handleOnChange} value={this.state.email}/></br>
+
+                    <label>Password</label></br>
+                    <input type="password" id="password" onChange={this.handleOnChange} value={this.state.password} /></br>
+                </form>
+			</div>
+		);
 	}
 }
 
