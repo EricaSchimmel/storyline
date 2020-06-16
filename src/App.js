eb20ff68fe3eb6bb6efb743ea0555bb26e97025f
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-15 22:48:31 -0400

    created a NavContainer and LoginContainer and modified App.js to have a consistent import React line to match LoginContainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 
-class App extends React.Component {
+class App extends Component {
 	render() {
 		return (
 			<BrowserRouter>
---
+++ src/containers/LoginContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class LoginContainer extends Component {
+	render() {
+		return <div></div>;
+	}
+}
