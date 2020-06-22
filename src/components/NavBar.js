2e25b6e788e377cda517685e74e1bf6cb234b72c
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-22 00:04:35 -0400

    added a home link to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,10 @@ class NavBar extends Component {
 			<div id="navBar">
 				<ul>
 					<li>
+						<Link to={"/"}>Home</Link>
+					</li>
+
+					<li>
 						<Link to={"/login"}>Log In</Link>
 					</li>
 				</ul>
