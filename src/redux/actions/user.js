7883dea9818e8bd19dca1446a2522ab6bb600482
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 19:32:41 -0400

    removed the unused getCookie function from user.js in redux/actions

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-/*********************************
- *    Title: Get By Regex Value
- *    Source: https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
- *********************************/
-
-const getCookie = cookieName => {
-	// Match the cookie with regex
-	let cookieMatch = document.cookie.match(
-		"(^|;)\\s*" + cookieName + "\\s*=\\s*([^;]+)"
-	);
-
-	// Return the cookie if a match is found
-	return cookieMatch ? cookieMatch.pop() : "";
-};
-
 export const setUser = data => {
 	return { type: "SET_USER", data };
 };
