55bd8b7b2521b8ad77378aa2e64f48145234b7e8
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 19:53:58 -0400

    added basic actions without functionality in manageCharacter and manageStory

--- src/redux/reducers/manageCharacter.js
+++ src/redux/reducers/manageCharacter.js
@@ -1,4 +1,15 @@
 export default function manageCharacter(state = {}, action) {
 	switch (action.type) {
+		case "ADD_CHARACTER":
+			break;
+
+		case "REMOVE_CHARACTER":
+			break;
+
+		case "ADD_COMMENT":
+			break;
+
+		default:
+			return state;
 	}
 }
--- src/redux/reducers/manageStory.js
+++ src/redux/reducers/manageStory.js
@@ -1,4 +1,15 @@
 export default function manageStory(state = {}, action) {
 	switch (action.type) {
+		case "ADD_STORY":
+			break;
+
+		case "REMOVE_STORY":
+			break;
+
+		case "ADD_COMMENT":
+			break;
+
+		default:
+			return state;
 	}
 }
