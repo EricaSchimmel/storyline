b7716017f549fd1cb18b69da335de11cbd9723df
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 21:37:14 -0400

    created StoryInput.js and CharacterInput.js to handle the input for stories and characters

---
+++ src/components/CharacterInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Errors from "./Errors";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { addCharacter } from "../redux/actions/manageCharacter";
+
+class CharacterInput extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default CharacterInput;
---
+++ src/components/StoryInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+mport React, { Component } from "react";
+import Errors from "./Errors";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { addStory } from "../redux/actions/manageStory";
+
+class StoryInput extends Component {
+	render() {
+		return <div></div>;
+	}
+}
+
+export default StoryInput;
