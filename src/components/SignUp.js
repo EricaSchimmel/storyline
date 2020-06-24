c6cb1344e54f201a4fe525dbdf6621f415e105ae
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 20:11:37 -0400

    added html to render with attributes for events in SignUp.js including a link to the login page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,7 +18,63 @@ class SignUp extends Component {
 	}
 
 	render() {
-		return <div></div>;
+		return (
+			<div id="signup-container">
+				<Errors errors={this.state.errors} />
+
+				<form onSubmit={this.handleOnSubmit}>
+					<label>Username</label>
+					<br />
+					<input
+						type="text"
+						id="username"
+						name="username"
+						onChange={this.handleOnChange}
+						value={this.state.email}
+					/>
+					<br />
+
+					<label>Email</label>
+					<br />
+					<input
+						type="email"
+						id="email"
+						name="email"
+						onChange={this.handleOnChange}
+						value={this.state.email}
+					/>
+					<br />
+
+					<label>Password</label>
+					<br />
+					<input
+						type="password"
+						id="password"
+						name="password"
+						onChange={this.handleOnChange}
+						value={this.state.password}
+					/>
+
+					<label>Confirm Password</label>
+					<br />
+					<input
+						type="password"
+						id="password_confirmation"
+						name="password_confirmation"
+						onChange={this.handleOnChange}
+						value={this.state.password_confirmation}
+					/>
+					<br />
+
+					<input type="submit" value="Log In" />
+				</form>
+
+				<p>
+					Already have an account?
+					<Link to={"/login"}>Click here to login!</Link>
+				</p>
+			</div>
+		);
 	}
 }
 
