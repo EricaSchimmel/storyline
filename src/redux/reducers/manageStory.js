ac333848a7536cae9c155a69715cc99b5585abba
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-24 15:44:52 -0400

    added functionality to ADD_STORY in manageStory reducer

--- src/redux/reducers/manageStory.js
+++ src/redux/reducers/manageStory.js
@@ -1,7 +1,14 @@
-export default function manageStory(state = {}, action) {
+export default function manageStory(state = { stories: [] }, action) {
 	switch (action.type) {
 		case "ADD_STORY":
-			break;
+			const story = {
+				id: action.id,
+				user_id: action.user_id,
+				title: action.title,
+				summary: action.summary,
+			};
+
+			return { ...state, stories: [...state.stories, story] };
 
 		case "REMOVE_STORY":
 			break;
