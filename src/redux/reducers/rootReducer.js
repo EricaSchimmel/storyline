81f0ba320a0055c37a114eeaf7c4a0b454eba127
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 21:38:29 -0400

    added manageStory and manageCharacter for rootReducer.js in reducers

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import manageUser from "./manageUser";
+import manageStory from "./manageStory";
+import manageCharacter from "./manageCharacter";
 
 const rootReducer = combineReducers({
 	manageUser,
+	manageStory,
+	manageCharacter,
 });
 
 export default rootReducer;
