43261f45556a0a7a953efde2482e3a525cdacf16
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-23 20:03:38 -0400

    created a SignUp component with a initial state

---
+++ src/components/SignUp.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			username: "",
+			email: "",
+			password: "",
+			password_confirmation: "",
+			errors: [],
+		};
+	}
+
+	render() {
+		return <div></div>;
+	}
+}
+
+export default SignUp;
