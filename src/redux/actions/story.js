59c4d9325f6a83a418ed953345326d97d3bcb440
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-24 15:36:23 -0400

    added a addStory and createStory function to story.js in redux/actions/story.js

--- src/redux/actions/story.js
+++ src/redux/actions/story.js
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+export const createStory = data => {
+	return { type: "ADD_STORY", data };
+};
+
+export const addStory = state => {
+	return dispatch => {
+		return axios.post("/stories", state).then(data => {
+			createStory(data);
+		});
+	};
+};
