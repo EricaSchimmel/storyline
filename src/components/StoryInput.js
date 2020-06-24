cf7fe285aeacee1808f808653cacb530c1c06023
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-24 15:04:28 -0400

    added constructor with initial state in StoryInput along with html to go with making the form input

--- src/components/StoryInput.js
+++ src/components/StoryInput.js
@@ -6,8 +6,43 @@ import { connect } from "react-redux";
 import { addStory } from "../redux/actions/manageStory";
 
 class StoryInput extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			title: "",
+			summary: "",
+		}
+	}
+	
 	render() {
-		return <div></div>;
+		return (
+		<div>
+			<form>
+				<label>Title</label>
+				<br />
+				<input
+					type="text"
+					id="title"
+					name="title"
+					onChange={this.handleOnChange}
+					value={this.state.title}
+				/>
+				<br />
+
+				<label>Summary</label>
+				<br />
+				<textarea
+					name="summary"
+					id="summary"
+					onChange={this.handleOnChange}
+					value={this.state.summary}
+				/>
+				<br />
+
+				<input type="submit" value="Submit Story" />
+			</form>
+		</div>
+		);
 	}
 }
 
