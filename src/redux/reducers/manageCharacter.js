e843bb2429c91dd7aa8ff6b277a79dd19205d924
OccassionalDev <ericaschimmel01@gmail.com>
2020-06-25 22:14:39 -0400

    added functionality to add and remove character actions in manageCharacter.js

--- src/redux/reducers/manageCharacter.js
+++ src/redux/reducers/manageCharacter.js
@@ -1,10 +1,25 @@
-export default function manageCharacter(state = {}, action) {
+export default function manageCharacter(state = { characters: [] }, action) {
 	switch (action.type) {
 		case "ADD_CHARACTER":
-			break;
+			const character = {
+				id: action.id,
+				story_id: action.story_id,
+				user_id: action.user_id,
+				name: action.name,
+				age: action.age,
+				gender: action.gender,
+				species: action.species,
+				personality: action.personality,
+				overview: action.overview,
+			};
+
+			return { ...state, characters: [...state.characters, character] };
 
 		case "REMOVE_CHARACTER":
-			break;
+			const characters = state.characters.filter(
+				character => character.id !== action.id
+			);
+			return { ...state, characters: [characters] };
 
 		case "ADD_COMMENT":
 			break;
