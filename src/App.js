0edaa52a0e303a436f576a8360d479e43aac33ea
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-09 23:03:48 -0400

    created a modules folder and created a history module to be imported around

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import "./stylesheets/App.css";
+
 import Home from "./containers/Home";
 import LoginContainer from "./containers/LoginContainer";
 import SignUpContainer from "./containers/SignUpContainer";
+
 import { BrowserRouter, Route } from "react-router-dom";
+import history from "./modules/history";
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
+			<BrowserRouter history={history}>
 				<Route path="/" exact component={Home} />
 				<Route path="/signup" exact component={SignUpContainer} />
 				<Route path="/login" exact component={LoginContainer} />
