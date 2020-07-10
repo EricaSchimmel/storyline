14bbb68452faee82e7d55acc30f3661658d959cd
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-09 23:08:42 -0400

    stopped passing history as a prop to NavBar in Login and SignUp containers

--- src/containers/SignUpContainer.js
+++ src/containers/SignUpContainer.js
@@ -15,7 +15,6 @@ class SignUpContainer extends Component {
 				<NavBar
 					currentUser={this.props.currentUser}
 					logout={this.props.logout}
-					history={history}
 				/>
 				<SignUp
 					currentUser={this.props.currentUser}
