0caee9a9e25ad4d9d68671bbf765ba2d64eef175
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-10 00:59:30 -0400

    added all routes to be added in App.js

--- src/App.js
+++ src/App.js
@@ -15,9 +15,21 @@ class App extends Component {
 				<Route path="/" exact component={Home} />
 				<Route path="/signup" exact component={SignUpContainer} />
 				<Route path="/login" exact component={LoginContainer} />
-				{/* <Route path="/dashboard" exact component={} />
-				<Route path="/stories" component={} />
-				<Route path="/characters" component={} /> */}
+
+				{/* User Pages */}
+				{/* <Route path="/dashboard" exact component={} /> */}
+				{/* <Route path="/users/:id" component={} /> */}
+				{/* <Route path="/users/:id/stories" component={} /> */}
+				{/* <Route path="/users/:id/characters" component={} /> */}
+
+				{/* Stories */}
+				{/* <Route path="/stories/:id" component={} /> */}
+				{/* <Route path="/stories/:id/new" component={} /> */}
+				{/* <Route path="/stories/:id/edit" component={} /> */}
+
+				{/* Characters */}
+				{/* <Route path="/stories/:story_id/characters/new" component={} /> */}
+				{/* <Route path="/characters/:id/edit" component={} /> */}
 			</Router>
 		);
 	}
