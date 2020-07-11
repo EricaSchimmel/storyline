93cd1f4691e0ca5501c86acf38d638415201fb0c
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-10 23:51:12 -0400

    removed unused functions from story actions

--- src/redux/actions/story.js
+++ src/redux/actions/story.js
@@ -8,35 +8,18 @@ export const addStories = data => {
 	return { type: "ADD_STORIES", data };
 };
 
-export const createStory = state => {
-	return dispatch => {
-		return axios.post("/stories", state).then(data => {
-			createStory(data);
-		});
-	};
-};
-
-export const updateStory = state => {
-	return axios.patch(`/stories/${state.story.id}`, state);
-};
-
-export const createComment = data => {
-	return { type: "ADD_COMMENT", data };
-};
-
-export const addComment = state => {
-	return dispatch => {
-		return axios
-			.post(`/stories/${state.story.id}/comments`, state.comment)
-			.then(data => {
-				createComment(data);
-			});
-	};
-};
+// export const createStory = state => {
+// 	return dispatch => {
+// 		return axios.post("/stories", state).then(data => {
+// 			createStory(data);
+// 		});
+// 	};
+// };
 
 export const fetchIndexStories = () => {
 	return dispatch => {
 		dispatch({ type: "LOADING_STORIES" });
+
 		return axios.get("/stories").then(data => {
 			dispatch(addStories(data.data));
 			return data;
@@ -47,6 +30,7 @@ export const fetchIndexStories = () => {
 export const fetchUserStories = user => {
 	return dispatch => {
 		dispatch({ type: "LOADING_STORIES" });
+
 		return axios.get(`/users/${user.id}/stories`).then(data => {
 			dispatch(addStories(data.data));
 			return data;
@@ -54,8 +38,13 @@ export const fetchUserStories = user => {
 	};
 };
 
-// export const deleteStory = state ={
-//     return dispatch => {
-//         return axios.delete(`/stories/${state.story.id}`);
-//     }
-// }
+export const fetchStory = storyId => {
+	return dispatch => {
+		dispatch({ type: "LOADING_STORY" });
+
+		return axios.get(`/stories/${storyId}`).then(data => {
+			dispatch(addStory(data.data));
+			return data;
+		});
+	};
+};
