1d167b01a657e65e0204a6f5adb7697c18ad5153
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-10 22:47:31 -0400

    added unique key to StoryCard in StoryList component

--- src/components/StoryList.js
+++ src/components/StoryList.js
@@ -4,7 +4,7 @@ import StoryCard from "./StoryCard";
 class StoryList extends Component {
 	render() {
 		const storyCards = this.props.stories.map(story => {
-			return <StoryCard story={story} />;
+			return <StoryCard story={story} key={story.id} />;
 		});
 
 		return <div>{storyCards}</div>;
