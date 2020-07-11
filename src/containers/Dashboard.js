a6324bae53dddf3f5f424b1dd4011b33b22ed12e
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-10 21:22:28 -0400

    mapped logout to props and included NavBar and requireLogin function from componentHelpers in Dashboard container

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+
+import NavBar from "../components/NavBar";
+
+import history from "../modules/history";
+import { requireLogin } from "../helpers/componentHelpers";
+
+import { logout } from "../redux/actions/user";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 class Dashboard extends Component {
 	render() {
 		return (
 			<div>
+				{requireLogin(this.props.currentUser)}
+
+				<NavBar
+					currentUser={this.props.currentUser}
+					logout={this.props.logout}
+					history={history}
+				/>
+
 				<h1>Welcome to your homepage, {this.props.currentUser}!</h1>
 				<h2>What would you like to do?</h2>
 
@@ -20,4 +36,10 @@ class Dashboard extends Component {
 	}
 }
 
-export default Dashboard;
+const mapStateToProps = state => {
+	return {
+		currentUser: state.currentUser,
+	};
+};
+
+export default connect(mapStateToProps, { logout })(Dashboard);
