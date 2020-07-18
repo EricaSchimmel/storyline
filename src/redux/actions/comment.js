30b0199350d8625d68649077ffb28a606b4253db
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-17 21:27:17 -0400

    added addComments and addComment to comment.js and added dispatch to fetchComments and postComment and added functionality to manageComments reducer

--- src/redux/actions/comment.js
+++ src/redux/actions/comment.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
+export const addComments = data => {
+	return { type: "ADD_COMMENTS", data };
+};
+
+export const addComment = data => {
+	return { type: "ADD_COMMENT", data };
+};
+
 export const fetchComments = (objType, objId) => {
-	return axios.get(`/${objType}/${objId}/comments`);
+	return dispatch => {
+		return axios.get(`/${objType}/${objId}/comments`);
+	};
 };
 
 export const postComment = (objType, objId, data) => {
-	return axios.post(`/${objType}/${objId}/comments`, data);
+	return dispatch => {
+		return axios.post(`/${objType}/${objId}/comments`, data);
+	};
 };
---
+++ src/redux/reducers/manageComments.js
@@ -0,0 +1,35 @@
+export default function manageComments(
+	state = { comments: [], canComment: false, loading: false },
+	action
+) {
+	switch (action.type) {
+		case "LOADING_COMMENTS":
+			return {
+				...state,
+				comments: [...state.comments],
+				canComment: state.canComment,
+				loading: true,
+			};
+
+		case "ADD_COMMENTS":
+			return {
+				...state,
+				comments: action.data.comments,
+				canComment: action.data.canComment,
+				loading: false,
+			};
+
+		case "ADD_COMMENT":
+			const comment = {
+				id: action.id,
+				story_id: action.story_id,
+				content: action.content,
+				user: action.user,
+			};
+
+			return { ...state, comments: [...state.comments, comments] };
+
+		default:
+			return state;
+	}
+}
