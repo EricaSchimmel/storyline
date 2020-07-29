7163e03c32d7fa421608285f646ac21987eb62d1
OccassionalDev <ericaschimmel01@gmail.com>
2020-07-29 00:02:14 -0400

    added a link on CommentCard to a user's show page

--- src/components/comments/CommentCard.js
+++ src/components/comments/CommentCard.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CommentCard = props => {
 	return (
 		<div>
-			<p>{props.comment.user.username}</p>
+			<p>
+				<Link to={`/users/${props.comment.user.id}`}>
+					{props.comment.user.username}
+				</Link>
+			</p>
 			<p>On {props.comment.created_at}</p>
 
 			<p>{props.comment.content}</p>
